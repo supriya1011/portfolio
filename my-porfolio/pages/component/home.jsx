4ebe858c79afcc2c0241ae985728ce645e60e6ae
supriya sahoo <supriyasahoo1011@gmail.com>
2023-01-09 14:27:54 +0530

    file added

--- my-porfolio/pages/component/home.jsx
+++ my-porfolio/pages/component/home.jsx
@@ -2,6 +2,7 @@
 const style={
     width:"150px",
     height:"90px",
+    margin:"80px",
 }
 
 
@@ -12,7 +13,7 @@ function Home() {
                 <div>
                     <img style={{style}} src="https://lh3.googleusercontent.com/a/AEdFTp7qN2v9nRMqf9qUwlIyz6OxGV1M5IxI5XasDMsepw=s96-c-rg-br100"></img>
                 </div>
-                <div></div>
+                <div><h1>Github_Account:  Supriya1011</h1></div>
                 <div></div>
             </div>
             <div></div>
