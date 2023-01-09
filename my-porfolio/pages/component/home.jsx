e6566f56deed36f7a0b84be9521b53479e3acd81
supriya sahoo <supriyasahoo1011@gmail.com>
2023-01-09 14:18:22 +0530

    file added

---
+++ my-porfolio/pages/component/home.jsx
@@ -0,0 +1,22 @@
+//import React from "react";
+const style={
+    width:"150px",
+    height:"90px",
+}
+
+
+function Home() {
+    return (
+        <div>
+            <div>
+                <div>
+                    <img style={{style}} src="https://lh3.googleusercontent.com/a/AEdFTp7qN2v9nRMqf9qUwlIyz6OxGV1M5IxI5XasDMsepw=s96-c-rg-br100"></img>
+                </div>
+                <div></div>
+                <div></div>
+            </div>
+            <div></div>
+        </div>
+    );
+}
+export default Home;
